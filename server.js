7ea6e781b6c1206696a02696f250c905f1e15241
Cristopher Ayala Paz <crisayala16@gmail.com>
2017-05-06 16:26:53 -0400

    updated

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');
 var path = require('path');
 
 var app = express();
-var port = 5000;
+var port = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -13,7 +13,6 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({
 	type: "application/vnd.api+json"
 }));
-app.set('port', (process.env.PORT || 5000));
 //Allows page access to external css files
 app.use(express.static(path.join(__dirname, '/app/public/css/')));
 //Html routes
